6d78c77db5c29e9ed74761461c55374fa161f5c6
Tommy Hodgins <tomhodgins@gmail.com>
2018-04-13 20:39:12 -0400

    Refactoring to use reduce()

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 export default (selector, option) => {
 
-  let styles = ''
-  let count = 0
-
   const features = {
     width: tag => {
       const computed = getComputedStyle(tag)
       tag.style.width = 'inherit'
       const width = parseInt(computed.getPropertyValue('border-left-width'), 10)
-                   + parseInt(computed.getPropertyValue('padding-left'), 10)
-                   + tag.scrollWidth
-                   + parseInt(computed.getPropertyValue('padding-right'), 10)
-                   + parseInt(computed.getPropertyValue('border-right-width'), 10)
+        + parseInt(computed.getPropertyValue('padding-left'), 10)
+        + tag.scrollWidth
+        + parseInt(computed.getPropertyValue('padding-right'), 10)
+        + parseInt(computed.getPropertyValue('border-right-width'), 10)
       tag.style.width = ''
       return `width: ${width}px;`
     },
@@ -19,10 +16,10 @@ export default (selector, option) => {
       const computed = getComputedStyle(tag)
       tag.style.height = 'inherit'
       const height = parseInt(computed.getPropertyValue('border-top-width'), 10)
-                   + parseInt(computed.getPropertyValue('padding-top'), 10)
-                   + tag.scrollHeight
-                   + parseInt(computed.getPropertyValue('padding-bottom'), 10)
-                   + parseInt(computed.getPropertyValue('border-bottom-width'), 10)
+        + parseInt(computed.getPropertyValue('padding-top'), 10)
+        + tag.scrollHeight
+        + parseInt(computed.getPropertyValue('padding-bottom'), 10)
+        + parseInt(computed.getPropertyValue('border-bottom-width'), 10)
       tag.style.height = ''
       return `height: ${height}px;`
     },
@@ -31,17 +28,19 @@ export default (selector, option) => {
     }
   }
 
-  document.querySelectorAll(selector).forEach(tag => {
+  return Array.from(document.querySelectorAll(selector))
 
-    const attr = selector.replace(/\W/g, '')
-    const evaluated = features[option](tag)
+    .reduce((styles, tag, count) => {
 
-    tag.setAttribute(`data-${attr}`, count)
-    styles += `${selector}[data-${attr}="${count}"] { ${evaluated} }\n`
-    count++
+      const attr = selector.replace(/\W/g, '')
+      const evaluated = features[option](tag)
 
-  })
+      tag.setAttribute(`data-${attr}`, count)
+      styles += `${selector}[data-${attr}="${count}"] { ${evaluated} }\n`
+      count++
 
-  return styles
+      return styles
+
+    }, '')
 
 }
